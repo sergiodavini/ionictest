24c6ffd91b21e0e3642aa2036d19135c19dd055d
Sergio Davini <sergiodavini@gmail.com>
2018-11-09 22:44:59 +0100

    Fix build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  secretid: '079bfb986ac44e8da6d70c9c53381616',
   clientid: '171b8d2440a54ce68a6f88a07a29d526',
   redirecturi: 'http://localhost:4500/callback'
 };
