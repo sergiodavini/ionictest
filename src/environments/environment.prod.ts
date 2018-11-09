9bf2a119be5305e2137f9b4944fd6b6be0bb462e
Shinteck srl <s.davini@shinteck.it>
2018-11-09 17:22:58 +0100

    Configurazione produzione

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+	production: true,
+	clientid: '171b8d2440a54ce68a6f88a07a29d526',
+	redirecturi: 'https://coverify.herokuapp.com/callback',
+	scopes: 'user-read-private user-read-email'
 };
